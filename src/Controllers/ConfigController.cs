722cb95735b621bafd343ec69ef6198fcd59967c
tomas-mota <tomasrebelomota@gmail.com>
2020-11-17 22:57:10 +0100

    Base project

---
+++ src/Controllers/ConfigController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace dotnet_sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigController : ControllerBase
+    {
+        private readonly string configVar;
+
+        public ConfigController(IConfiguration Configuration)
+        {
+            configVar = Configuration["ConfigVar"];
+        }
+
+        [HttpGet]
+        public string GetConfig()
+        {
+            return configVar;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace dotnet_sample
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
